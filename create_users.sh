cf9a94fe263497ab8c8f3c615b5dee688ccfe3bc
Mr_milt <milton@marjasin.se>
2026-05-12 19:08:22 +0200

    created the code

---
+++ create_users.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+user_id=$(id -u)
+
+# echo $user_id
+
+if [[ $user_id -ne 0 ]]; then
+    echo "Need to run as root"
+    exit 1
+fi
+
+echo "is root"
+
+for usr in $@; do
+    echo "creating user $usr"
+    adduser --gecos "" $usr 
+    echo creating Documents, Download and Work dirs
+    mkdir -p "/home/$usr/Documents" "/home/$usr/Downloads" "/home/$usr/Work"
+    echo "adding user to user log file" 
+    echo "$usr" >> /log/users
+    echo "Välkommen $usr" >> /home/$usr/welcome.txt
+    echo " " >> /home/$usr/welcome.txt
+    cat /log/users >> /home/$usr/welcome.txt
+    echo "handeling user perms"
+    chown $usr /home/$usr/* && chgrp $usr /home/$usr/* && chmod u=rwx,o=rwx /home/$usr/* 
+    echo $(ls -lR /home/$usr/)
+    
+done
