abffee9cc5ee5632f4aa5dd242833434426c15b6
Mr_milt <milton@marjasin.se>
2026-05-12 22:18:15 +0200

    Ny videprov

--- create_users.sh
+++ create_users.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-user_id=$(id -u)
+# get user id of curent user
+user_id=$(id -u) 
 
 # echo $user_id
 
+# checks if user id is not 0
 if [[ $user_id -ne 0 ]]; then
     echo "Need to run as root"
     exit 1
 fi
 
 echo "is root"
-mkdir /log/ 
+mkdir /log/ # skapar ifall finns
 
 echo ""
 
+# loop vi igen arguemt som gavs
 for usr in $@; do
     echo "creating user $usr"
-    adduser --disabled-password --gecos "" $usr 
+    adduser --disabled-password --comment "" $usr 
     echo creating Documents, Download and Work dirs
     mkdir -p "/home/$usr/Documents" "/home/$usr/Downloads" "/home/$usr/Work"
     echo "adding user to user log file" 
